d5fbbc6f00099d48cdecae32dac3db2feb458351
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2020-05-06 12:23:01 +0300

    remove timers from piece loader

--- services/piece_pool.go
+++ services/piece_pool.go
@@ -3,11 +3,6 @@ package services
 import (
 	"io"
 	"sync"
-	"time"
-)
-
-const (
-	PIECE_TTL = 30
 )
 
 type PiecePool struct {
@@ -15,32 +10,17 @@ type PiecePool struct {
 	httppp *HTTPPiecePool
 	cpp    *CompletedPiecesPool
 	sm     sync.Map
-	mux    sync.Mutex
-	timers sync.Map
-	expire time.Duration
 }
 
 func NewPiecePool(cpp *CompletedPiecesPool, s3pp *S3PiecePool,
 	httppp *HTTPPiecePool) *PiecePool {
-	return &PiecePool{s3pp: s3pp, httppp: httppp, cpp: cpp, expire: time.Duration(PIECE_TTL) * time.Second}
+	return &PiecePool{s3pp: s3pp, httppp: httppp, cpp: cpp}
 }
 
 func (s *PiecePool) Get(src string, h string, p string, q string) (io.ReadSeeker, error) {
-	v, _ := s.sm.LoadOrStore(p, NewPieceLoader(s.cpp, s.s3pp, s.httppp, src, h, p, q))
-	t, tLoaded := s.timers.LoadOrStore(p, time.NewTimer(s.expire))
-	timer := t.(*time.Timer)
-	if !tLoaded {
-		go func() {
-			<-timer.C
-			s.sm.Delete(p)
-			s.timers.Delete(p)
-			v.(*PieceLoader).Clear()
-		}()
-	} else {
-		s.mux.Lock()
-		timer.Reset(s.expire)
-		s.mux.Unlock()
+	v, loaded := s.sm.LoadOrStore(p, NewPieceLoader(s.cpp, s.s3pp, s.httppp, src, h, p, q))
+	if !loaded {
+		defer s.sm.Delete(p)
 	}
-
 	return v.(*PieceLoader).Get()
 }
