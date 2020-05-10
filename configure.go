ea4b067967fcdb1f14cbf6ec23f3dcbd1078e478
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2020-05-11 00:05:08 +0300

    tune

--- configure.go
+++ configure.go
@@ -59,7 +59,7 @@ func run(c *cli.Context) error {
 	pp := s.NewPiecePool(cpp, s3pp, httppp)
 
 	// Setting Leaky Buffer
-	lb := s.NewLeakyBuffer(50, 128*1024)
+	lb := s.NewLeakyBuffer(100, 128*1024)
 
 	// Setting Reader Pool
 	rp := s.NewReaderPool(pp, mip, ttp, lb)
